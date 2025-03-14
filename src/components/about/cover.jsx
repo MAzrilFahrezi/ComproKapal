143e81b2fd8267e25c9dc264b206f43c6ee30f22
MuhammadDody2002 <muhammaddody05@mhs.mdp.ac.id>
2025-03-14 09:36:29 +0700

    update about

--- src/components/about/cover.jsx
+++ src/components/about/cover.jsx
@@ -6,12 +6,12 @@ const Cover = () => {
             <img 
                 src="https://img.freepik.com/premium-photo/cargo-ship-ocean-with-containers_951562-15636.jpg" 
                 alt="Kapal Kargo" 
-                className="w-full h-64 object-cover rounded-none"
+                className="w-full h-80 object-cover rounded-none"
             />
             <div className="absolute inset-0 flex flex-col items-start justify-center p-8 bg-black bg-opacity-50">
                 <h2 className="text-5xl font-bold text-white mb-4">About</h2>
                 <p className="text-lg text-white">
-                    Take the complexity out of customs Freightddd Solutions with customs brokerage services
+                    Take the complexity out of customs Freight Solutions with customs brokerage services
                 </p>
             </div>
         </section>
